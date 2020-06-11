eedfa3c0a9d6ce083d3281ff33c38ad7f6c703f4
Lee-do-yeong <64951097+Lee-do-yeong@users.noreply.github.com>
2020-06-11 14:00:49 +0900

    Add files via upload

---
+++ main.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+enum Color  //콘솔에서 사용하는 색상
+{
+    BLACK, BLUE, GREEN, CYAN,
+    RED, MAGENTA, BROWN, LIGHTGRAY,
+    DARKGRAY, LIGHTBLUE, LIGHTGREEN, LIGHTCYAN,
+    LIGHTRED, LIGHTMAGENTA, YELLOW, WHITE
+};
+
+using namespace std;
+
+#define UP 72
+#define DOWN 80
+#define RIGHT 77
+#define LEFT 75
+#define CHOICE 13
+
+void setColor(int back, int text);
+void init();
+void gotoxy(int x, int y);
+void introGame(); // 게임 시작화면 함수
+int keyControl(); // 방향키로 메뉴 선택 함수
+int menuChoice(); // 시작화면 메뉴 선택 함수
+void gameImage(); // 게임화면 이미지 출력 함수
+void gameRule(); // 게임 룰 화면 함수
+void player(int x, int y); // 컴퓨터 플레이어 이미지 출력 함수
+void bettingNumber(); // 베팅 메뉴 선택 함수
+int bettingMenuChoice(); // 베팅 메뉴 이름 출력 함수
+void victoryPrint(); // 승리 시 화면 출력
+void defeatPrint();  // 패배 시 화면 출력
